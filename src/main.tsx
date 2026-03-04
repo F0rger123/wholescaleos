a197edcaef0f816f84bc4c8b8b6a893007cf1b9c
F0rger123 <drummerforger@gmail.com>
2026-03-04 14:32:30 -0500

    Fix #67

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App } from "./App";
-import { ThemeProvider } from "./components/ThemeProvider";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+// Add a test div to prove React is working
+const testDiv = document.createElement('div');
+testDiv.id = 'test-div';
+testDiv.style.position = 'fixed';
+testDiv.style.top = '0';
+testDiv.style.left = '0';
+testDiv.style.right = '0';
+testDiv.style.backgroundColor = 'green';
+testDiv.style.color = 'white';
+testDiv.style.padding = '20px';
+testDiv.style.zIndex = '99999';
+testDiv.innerText = '✅ main.tsx loaded - ' + new Date().toLocaleTimeString();
+document.body.appendChild(testDiv);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
