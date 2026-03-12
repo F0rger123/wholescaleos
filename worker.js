b8e128df398c870978b3cd72214bf5a4ee4a0d8e
F0rger123 <drummerforger@gmail.com>
2026-03-12 19:23:07 -0400

    Build worked but website had error page

--- worker.js
+++ worker.js
@@ -1,39 +1,11 @@
 export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    
+  async fetch(request, env) {
     try {
-      // Try to serve the requested asset first
-      let response = await env.ASSETS.fetch(request);
-      
-      // If the asset exists, return it
-      if (response.status !== 404) {
-        return response;
-      }
-      
-      // Otherwise, serve index.html for SPA routing
-      response = await env.ASSETS.fetch(new URL('/index.html', request.url));
-      return new Response(response.body, {
-        ...response,
-        headers: {
-          ...response.headers,
-          'Content-Type': 'text/html',
-        },
-      });
+      // Just serve index.html for all requests
+      const url = new URL('/index.html', request.url);
+      return await env.ASSETS.fetch(url);
     } catch (error) {
-      // If everything fails, try to serve index.html directly
-      try {
-        const indexResponse = await env.ASSETS.fetch(new URL('/index.html', request.url));
-        return new Response(indexResponse.body, {
-          ...indexResponse,
-          headers: {
-            ...indexResponse.headers,
-            'Content-Type': 'text/html',
-          },
-        });
-      } catch (e) {
-        return new Response('Application Error', { status: 500 });
-      }
+      return new Response('Application Error: ' + error.message, { status: 500 });
     }
   }
-};
+}
